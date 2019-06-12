52f638e4acc0853178dbb8f7f7a03fe3bc47e6dc
Deril Alwinda <deril@yahoo.com>
2019-06-12 19:12:03 +0700

    Add project files.

---
+++ MineskiPortal/Controllers/RegisterController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MineskiPortal.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public PartialViewResult Index()
+        {
+            return new PartialViewResult
+            {
+                ViewName = "Index",
+                ViewData = this.ViewData
+            };
+        }
+    }
+}
