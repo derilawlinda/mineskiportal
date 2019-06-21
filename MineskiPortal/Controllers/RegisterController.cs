f209d36cdebd04058b4df0a4b50e59bce6b3780f
Deril Alwinda <deril@yahoo.com>
2019-06-21 13:22:56 +0700

    Upload files

--- MineskiPortal/Controllers/RegisterController.cs
+++ MineskiPortal/Controllers/RegisterController.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using LiteDB;
 using Microsoft.AspNetCore.Mvc;
 using MineskiPortal.Models;
+using System.Web;
+using System.IO;
+using Microsoft.AspNetCore.Http;
 
 namespace MineskiPortal.Controllers
 {
@@ -59,10 +62,66 @@ namespace MineskiPortal.Controllers
             }
             catch(Exception exception)
             {
-
+                return Json(new { success = false, responseText = exception.Message });
             }
 
-            return Json(String.Format("'Success': 'true'"));
+            return Json(new { success = true });
+        }
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return Content("file not selected");
+
+            var path = Path.Combine(
+                        Directory.GetCurrentDirectory(), "wwwroot","uploads",
+                        file.FileName);
+
+            using (var stream = new FileStream(path, System.IO.FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return RedirectToAction("Files");
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> NonEventCreate(string Name, string Address, string Email, string Gender, string MobileNumber,string IdNumber, string MaritalStatus, IFormFile Photo)
+        {
+
+            try
+            {
+
+                using (var db = new LiteDatabase(@"Mineski.db"))
+                {
+                    // Get customer collection
+                    var customerEvents = db.GetCollection<CustomerNonEvent>("customerNonEvents");
+                    if(Photo != null)
+                    {
+                        IActionResult actionResult = await UploadFile(Photo);
+                    }
+
+                    var customerNonEventData = new CustomerNonEvent
+                    {
+                        Address = Address,
+                        Email = Email,
+                        Gender = Gender,
+                        MobileNumber = MobileNumber,
+                        Name = Name,
+                        IdNumber = IdNumber,
+                        MaritalStatus = MaritalStatus,
+                        Photo = Photo.FileName
+                    };
+
+                    // Insert new customer document (Id will be auto-incremented)
+                    customerEvents.Insert(customerNonEventData);
+                }
+            }
+            catch (Exception exception)
+            {
+                return Json(new { success = false, responseText = exception.Message });
+            }
+
+            return Json(new { success = true });
         }
     }
 }
---
+++ MineskiPortal/Models/CustomerNonEvent.cs
@@ -0,0 +1,16 @@
+﻿namespace MineskiPortal.Models
+{
+    public class CustomerNonEvent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string IdNumber { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string MaritalStatus { get; set; }
+        public string MobileNumber { get; set; }
+        public string Photo { get; set; }
+
+    }
+}
